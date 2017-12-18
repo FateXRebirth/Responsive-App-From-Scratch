557954498c947b4a1913925eb2b202d6b0b02c04
Kevin Peng <FateXRebirth@gmail.com>
2017-12-18 14:51:28 +0800

    Hotai PC index

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const gulp = require('gulp');
+const gutil = require('gulp-util');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
 
 // Compile Sass
 gulp.task('sass', () => {
     return gulp.src(['src/scss/*.scss'])
+    .pipe(plumber(function(error) {
+        gutil.beep();
+        gutil.log(gutil.colors.red(error.message));
+        this.emit('end');
+    }))
     .pipe(sass())
     .pipe(gulp.dest('src/css'))
     .pipe(browserSync.stream());
