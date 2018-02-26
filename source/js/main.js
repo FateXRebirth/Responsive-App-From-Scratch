9e0459002ac8b9ec1a2202e5a8a89f6cc363aa43
FateXRebirth <FateXRebirth@gmail.com>
2018-02-26 13:51:59 +0800

    Split Landing Page

--- source/js/main.js
+++ source/js/main.js
@@ -21,3 +21,23 @@ $(document).ready(function () {
 $(window).on('load', function() {
     console.log("Loaded");
 })
+
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector('.container');
+
+left.addEventListener('mouseenter', () => {
+    container.classList.add('hover-left');
+})
+
+left.addEventListener('mouseleave', () => {
+    container.classList.remove('hover-left');
+})
+
+right.addEventListener('mouseenter', () => {
+    container.classList.add('hover-right');
+})
+
+right.addEventListener('mouseleave', () => {
+    container.classList.remove('hover-right');
+})
